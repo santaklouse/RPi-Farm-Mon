8255764fe5569668869d6a248b40c8e897c28c63
Your Name <you@example.com>
2017-10-07 17:31:27 +0900

    updated.

--- sample/sample3.sh
+++ sample/sample3.sh
@@ -1,13 +1,14 @@
 DATE=`date '+%Y/%m/%d %H:%M:%S'`
-SUBJECT="Picture: ${DATE}"
-MAILBODY=/tmp/content.txt
+SUBJECT="Environment Data: ${DATE}"
+MAILBODY=/tmp/status.txt
 IMAGE=/tmp/image.jpg
 DESTINATION="EXAMPLE@EXAMPLE.COM"    # < Change to your mail address.
 
 echo "Date: $DATE" > $MAILBODY
+python /home/pi/RPi-Farm-Mon/lib/sht31.py >> $MAILBODY
+python /home/pi/RPi-Farm-Mon/lib/tsl2561.py >> $MAILBODY
 fswebcam -r 640x480 --jpeg 95 $IMAGE >/dev/null
-
+    
 mutt -s "$SUBJECT" $DESTINATION -a $IMAGE < $MAILBODY
-
 rm $MAILBODY
 rm $IMAGE
