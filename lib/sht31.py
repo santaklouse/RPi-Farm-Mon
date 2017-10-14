ac2bdabdf76c23b2d3a8186ac6af0946afd92e52
mbuzz15 <takashi.miyoshi@gmail.com>
2017-10-14 23:53:09 +0900

    Update sht31.py

--- lib/sht31.py
+++ lib/sht31.py
@@ -1,11 +1,12 @@
 import smbus
 import time
 
+
 class SHT31:
     def __init__(self, address, channel):
-        self.address    = address
-        self.channel    = channel
-        self.bus        = smbus.SMBus(self.channel)
+        self.address = address
+        self.channel = channel
+        self.bus = smbus.SMBus(self.channel)
         self.bus.write_i2c_block_data(self.address, 0x2C, [0x06])
         time.sleep(0.5)
 
@@ -13,7 +14,7 @@ class SHT31:
         raw = self.bus.read_i2c_block_data(self.address, 0x00, 6)
         temp = raw[0] * 256 + raw[1]
         cTemp = -45 + (175 * temp / 65535.0)
-        #fTemp = -49 + (315 * temp / 65535.0)
+        fTemp = -49 + (315 * temp / 65535.0)
         humidity = 100 * (raw[3] * 256 + raw[4]) / 65535.0
 
         return (cTemp, humidity)
@@ -21,5 +22,5 @@ class SHT31:
 if __name__ == '__main__':
     sensor = SHT31(0x45, 1)
     data = sensor.read()
-    print('Temperature: %s' % round(data[0],2))
-    print('Humidity:    %s' % round(data[1],2))
+    print('Temperature: %s' % round(data[0], 2))
+    print('Humidity:    %s' % round(data[1], 2))
