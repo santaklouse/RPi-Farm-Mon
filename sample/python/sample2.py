c1b39190a7951f7dabc2663f2cbffb59aeb98e30
mbuzz15 <takashi.miyoshi@gmail.com>
2017-10-14 21:02:26 +0900

    Update sample2.py

--- sample/python/sample2.py
+++ sample/python/sample2.py
@@ -1,12 +1,28 @@
 import os
+import time
+import datetime
 import boto3
 
-filename = 'image.jpg'
 bucket_name = 'monitor-u7wdtw9b'
+folder_name = 'raspberrypi'
+webcam_device = '/dev/video0'
+smart_plug_ip_address = '192.168.100.51'
+rightnow = datetime.datetime.now().strftime('%Y%m%d-%H%M')
 
-command = 'fswebcam -r 640x480 --jpeg 95 /tmp/' + filename
+command = "/usr/local/bin/pyhs100 --ip=%s on" % smart_plug_ip_address
 os.system(command)
-s3 = boto3.client('s3')
-s3.upload_file('/tmp/' + filename, bucket_name, filename)
-command = 'rm /tmp/' + filename
+time.sleep(3)
+
+command = "fswebcam -d %s -r 1280x960 --jpeg 95 /tmp/latest.jpg" % webcam_device
+os.system(command)
+time.sleep(1)
+
+command = "/usr/local/bin/pyhs100 --ip=%s off" % smart_plug_ip_address
+os.system(command)
+
+s3 = boto3.resource('s3')
+s3.meta.client.upload_file("/tmp/latest.jpg", bucket_name, "%s/latest.jpg" % folder_name, ExtraArgs={'ContentType': "image/jpeg", 'ACL': "public-read"} )
+s3.meta.client.upload_file("/tmp/latest.jpg", bucket_name, "%s/%s.jpg" % (folder_name, rightnow), ExtraArgs={'ContentType': "image/jpeg", 'ACL': "public-read"} )
+
+command = "rm /tmp/latest.jpg"
 os.system(command)
